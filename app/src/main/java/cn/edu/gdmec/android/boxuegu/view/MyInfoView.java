8f73d7fd680b7b4f90c5f1fff21e990cfc7cf105
Easierman <554925583@qq.com>
2018-04-15 16:30:20 +0800

    第五章over

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/view/MyInfoView.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/view/MyInfoView.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.activity.LoginActivity;
 import cn.edu.gdmec.android.boxuegu.activity.SettingActivity;
+import cn.edu.gdmec.android.boxuegu.activity.UserInfoActivity;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
 /**
@@ -57,8 +58,10 @@ public class MyInfoView {
                 //判断是否已经登录
                 if (readLoginStatus()){
                     //已登录转跳到个人资料界面
-                    Intent intent = new Intent(mContext,SettingActivity.class);
-                    mContext.startActivityForResult(intent,1);
+//                    Intent intent = new Intent(mContext,SettingActivity.class);
+//                    mContext.startActivityForResult(intent,1);
+                    Intent intent = new Intent(mContext, UserInfoActivity.class);
+                    mContext.startActivity(intent);
                 }else{
                     //未登录转跳到登录页面
                     Intent intent = new Intent(mContext,LoginActivity.class);
